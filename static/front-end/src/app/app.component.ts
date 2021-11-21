f4cdfd17e84d123d2473f4e5528aca90ee640357
Fahad Malik <unexpectedprogrammer@gmail.com>
2021-11-22 03:30:28 +0500

    Checking the typescript file config with the django rendering template and simple html file

--- static/front-end/src/app/app.component.ts
+++ static/front-end/src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'front-end';
+
+  private username:String = 'Ali'
+
+  getUserName():String {
+    return this.username;
+  }
+
 }
